6933de0e2990515dd62b52f46dc866d03149f7f8
Avstel <it.nathan.demissie@gmail.com>
2025-11-02 10:10:25 +0300

    Create views.py

---
+++ listings/views.py
@@ -0,0 +1,8 @@
+from listings.tasks import send_booking_confirmation_email
+
+class BookingViewSet(viewsets.ModelViewSet):
+    ...
+
+    def perform_create(self, serializer):
+        booking = serializer.save()
+        send_booking_confirmation_email.delay(booking.customer.email, booking.id)
